df754400d9af1a735914874981143e3230f9d0c2
Paulo Sabra <paulosabra.dev@gmail.com>
2025-06-10 18:34:29 -0300

    widget(button): create NuButton component

---
+++ lib/components/button.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:nubank/core/themes/colors.dart';
+
+class NuButton extends StatelessWidget {
+  const NuButton.primary({
+    super.key,
+    required this.text,
+    this.icon,
+    this.onPressed,
+    this.isExpanded = false,
+  }) : style = const ButtonStyle(
+         backgroundColor: WidgetStatePropertyAll(AppColors.primary),
+         foregroundColor: WidgetStatePropertyAll(AppColors.white),
+         elevation: WidgetStatePropertyAll(0),
+         padding: WidgetStatePropertyAll(
+           EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+         ),
+         shape: WidgetStatePropertyAll(
+           RoundedRectangleBorder(
+             borderRadius: BorderRadius.all(Radius.circular(24)),
+           ),
+         ),
+       );
+
+  const NuButton.secondary({
+    super.key,
+    required this.text,
+    this.icon,
+    this.onPressed,
+    this.isExpanded = true,
+  }) : style = const ButtonStyle(
+         backgroundColor: WidgetStatePropertyAll(AppColors.neutralLight),
+         foregroundColor: WidgetStatePropertyAll(AppColors.black),
+         elevation: WidgetStatePropertyAll(0),
+         padding: WidgetStatePropertyAll(
+           EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+         ),
+         shape: WidgetStatePropertyAll(
+           RoundedRectangleBorder(
+             borderRadius: BorderRadius.all(Radius.circular(12)),
+           ),
+         ),
+       );
+
+  final String text;
+  final String? icon;
+  final ButtonStyle style;
+  final VoidCallback? onPressed;
+  final bool isExpanded;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: style,
+      child: Row(
+        mainAxisSize: isExpanded ? MainAxisSize.max : MainAxisSize.min,
+        children: [
+          if (icon == null)
+            const SizedBox()
+          else
+            Row(
+              children: [
+                SvgPicture.asset(icon ?? '', width: 24, height: 24),
+                const SizedBox(width: 16),
+              ],
+            ),
+          Text(
+            text,
+            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+          ),
+        ],
+      ),
+    );
+  }
+}
